6d6011909318213d09bb0699b9123c659440b39a
FabioJuchem <fabiojuchem@gmail.com>
2019-06-21 08:13:58 -0300

    Adicionado curso de front-end completo

---
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="estiloDoBotao" class="botao" :type="tipo" @click="disparaAcao()">{{rotulo}}</button>
+</template>
+
+<script>
+
+
+export default {
+  
+  props: {
+      tipo:{
+          required: true,
+          type: String
+      },
+      rotulo:{
+          required: true,
+          type: String
+      },
+      confirmacao: Boolean,
+      estilo: String
+
+  },
+
+  methods: {
+    disparaAcao() {
+      if (this.confirmacao) {
+        if (confirm("Confirma Operação?")) {
+          this.$emit("botaoAtivado");
+        }
+        return;
+      }
+      this.$emit("botaoAtivado");
+    }
+  },
+  computed: {
+
+      estiloDoBotao(){
+          if(this.estilo == 'padrao' || !this.estilo) return 'botao botao-padrao';
+          if(this.estilo == 'perigo') return 'botao botao-perigo';
+
+      }
+
+  }
+}
+</script>
+
+<style lang="sass">
+
+@import './Botao.scss';
+
+
+</style>
